2e550b47ca5de2ea7e5e30f14dbc5ca6dbfb6c74
Chokerino <43617111+Chokerino@users.noreply.github.com>
2019-10-08 13:11:39 +0530

    initial

---
+++ ml_protein.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import heapq
+#df = pd.read_excel (r'HiSeqV2.xlsx') #for an earlier version of Excel, you may need to use the file extension of 'xls'
+#print (df)
+import xlrd 
+book = xlrd.open_workbook('HiSeqV2.xlsx')
+sheet=[]
+max_list=[]
+print(type(book.sheets()))
+for x in book.sheets():
+	sheet.append(x)
+for i in range(1,sheet[0].ncols/2):
+	max_gene=[]
+	max_list_positions=[]
+	max_list=heapq.nlargest(20,sheet[0].col_values(i)[1:])
+	print(max_list)
+	print(type(max_list))
+	print(type(sheet[0].col_values(i)[1:]))
+	for j in max_list:
+		max_list_positions.append(sheet[0].col_values(i)[1:].index(j))
+	for j in max_list_positions:
+		max_gene.append(sheet[0].col_values(0)[1:][j])
+	plt.plot(max_gene,max_list,label=sheet[0].col_values(i)[0])
+	#print(sheet[0].col_values(i))
+   
+plt.savefig('my_figure.png', dpi=1000)
+plt.show()
+plt.close()
