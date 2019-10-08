b4b66fc6000061e8dfd9c4437a629be867e9ec16
Chokerino <43617111+Chokerino@users.noreply.github.com>
2019-10-08 15:36:34 +0530

    initial2

--- ml_protein.py
+++ ml_protein.py
@@ -7,20 +7,25 @@ import xlrd
 book = xlrd.open_workbook('HiSeqV2.xlsx')
 sheet=[]
 max_list=[]
+common=[]
+genes={}
 print(type(book.sheets()))
 for x in book.sheets():
 	sheet.append(x)
-for i in range(1,sheet[0].ncols/2):
+for i in range(1,sheet[0].ncols):
 	max_gene=[]
 	max_list_positions=[]
 	max_list=heapq.nlargest(20,sheet[0].col_values(i)[1:])
-	print(max_list)
-	print(type(max_list))
 	print(type(sheet[0].col_values(i)[1:]))
 	for j in max_list:
 		max_list_positions.append(sheet[0].col_values(i)[1:].index(j))
 	for j in max_list_positions:
 		max_gene.append(sheet[0].col_values(0)[1:][j])
+	for g in max_gene:
+		if g not in genes:
+			genes[g]=0
+		else:
+			genes[g]+=1		
 	plt.plot(max_gene,max_list,label=sheet[0].col_values(i)[0])
 	#print(sheet[0].col_values(i))
    
