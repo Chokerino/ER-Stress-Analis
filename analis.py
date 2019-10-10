311b352c1670ed95dd01c70e13063656d9b27856
Chokerino <43617111+Chokerino@users.noreply.github.com>
2019-10-10 19:51:21 +0530

    Graphed Expression

--- analis.py
+++ analis.py
@@ -14,4 +14,43 @@ with open("gene_max_list.json", 'r') as file:
 	plt.plot(gene_name,gene_number)
 	plt.savefig('most_occuring_max_genes.png', dpi=1000)
 	plt.show()
-	plt.close()	
+
+with open("gene_min_list.json", 'r') as file:
+	data = json.load(file)
+	max_genes=dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True)[:10])
+	print(max_genes)
+	gene_name=[]
+	gene_number=[]
+	for i in max_genes:
+		gene_name.append(i)
+		gene_number.append(max_genes[i])
+	plt.plot(gene_name,gene_number)
+	plt.savefig('least_occuring_min_genes.png', dpi=1000)
+	plt.show()
+
+			
+with open("gene_max_expression.json", 'r') as file:
+	data = json.load(file)
+	max_genes=dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True)[:10])
+	print(max_genes)
+	gene_name=[]
+	gene_number=[]
+	for i in max_genes:
+		gene_name.append(i)
+		gene_number.append(max_genes[i])
+	plt.plot(gene_name,gene_number)
+	plt.savefig('most_expressed_min_genes.png', dpi=1000)
+	plt.show()
+
+with open("gene_min_expression.json", 'r') as file:
+	data = json.load(file)
+	max_genes=dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True)[:10])
+	print(max_genes)
+	gene_name=[]
+	gene_number=[]
+	for i in max_genes:
+		gene_name.append(i)
+		gene_number.append(max_genes[i])
+	plt.plot(gene_name,gene_number)
+	plt.savefig('least_expressed_min_genes.png', dpi=1000)
+	plt.show()	
